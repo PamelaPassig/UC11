750f2fc8f8b25525f88dc83c96cc21bb8b51546c
Pamela passig <131300611+PamelaPassig@users.noreply.github.com>
2025-05-14 19:56:23 -0300

    Função de cadastramento 0.0

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -13,7 +13,19 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){        
-        
+        String sql = "INSERT INTO produtos (nome, valor, situacao) VALUES (?, ?, ?)";
+        }   
+        conn = new conectaDAO().connectDB();
+        try{
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getSituacao());
+        prep.execute();
+        JOptionPane.showMessageDialog(null,"Produto cadastrado");
+    }catch (Exception e){
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar: "+ e.getMessage());
+}
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
